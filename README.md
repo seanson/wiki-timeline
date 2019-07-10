414d95fc0b073916553c60ab7831126d9ee54d9b
seanson@users.noreply.github.com <seanson@users.noreply.github.com>
2019-07-10 14:32:18 +1000

    Website bump

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-not great
+# wiki-timeline
+
+Quick and dirty sample project for doing some wikipedia scraping and timeline testing.
+
+Code is untested and rough.
---
+++ precache-manifest.6459487d436813cfa463beeef48f9129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422eb87cc980ebc29cdf41a08c945227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f0f2d7a7a03f2ec3be9",
+    "url": "/static/css/2.b48b4a8b.chunk.css"
+  },
+  {
+    "revision": "9a54679b18b069b84d70",
+    "url": "/static/css/main.09329e4c.chunk.css"
+  },
+  {
+    "revision": "3f0f2d7a7a03f2ec3be9",
+    "url": "/static/js/2.72166171.chunk.js"
+  },
+  {
+    "revision": "9a54679b18b069b84d70",
+    "url": "/static/js/main.c7b85eff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
